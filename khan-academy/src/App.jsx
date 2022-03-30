5bb7af801fa6cdf09550dbede384f75ee6e73562
nHari9 <nharikishorereddy9@gmail.com>
2022-03-30 19:19:36 +0530

    home page initial commit

--- khan-academy/src/App.jsx
+++ khan-academy/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Navbar from './Components/Navbar/navbar'
+import Home from './Components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
     </div>
   )
 }
